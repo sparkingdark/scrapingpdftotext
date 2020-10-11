a96abc30a7ffa6f17d5f4c296aedaf5095199952
sparkingdark <debomastet335@gmail.com>
2020-10-11 09:09:38 +0530

    added'

--- load30.py
+++ load30.py
@@ -9,9 +9,6 @@ def getlinesasjson(paths,jsonfile):
     for i in all_pdfs:
         with open(paths+i,'r') as f:
             content = f.readlines()
-            for i in content:
-                if i==" \n" or i=="  \n" or "\n":
-                   content.remove(i)
             dic.update({i:"".join(content[:31])})
         f.close()
     with open(jsonfile,'w+') as p:
