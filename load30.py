ea667b222d35c7cddd90c73924db136ee62707f6
sparkingdark <debomastet335@gmail.com>
2020-10-11 08:58:48 +0530

    preprocessed text

--- load30.py
+++ load30.py
@@ -9,6 +9,9 @@ def getlinesasjson(paths,jsonfile):
     for i in all_pdfs:
         with open(paths+i,'r') as f:
             content = f.readlines()
+            for i in content:
+                if i==" \n" or i=="  \n" or "\n":
+                   content.remove(i)
             dic.update({i:content[:31]})
         f.close()
     with open(jsonfile,'w+') as p:
