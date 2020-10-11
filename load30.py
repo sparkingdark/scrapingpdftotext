ac08da82ffc6408f6cbbe86a6030f9a300a1603e
sparkingdark <debomastet335@gmail.com>
2020-10-11 08:48:07 +0530

    added json support

---
+++ load30.py
@@ -0,0 +1,18 @@
+import os 
+import json
+
+
+def getlinesasjson(paths,jsonfile):
+    all_pdfs = os.listdir(paths)
+    content = None
+    dic = {}
+    for i in all_pdfs:
+        with open(paths+i,'r') as f:
+            content = f.readlines()
+            dic.update({i:content[:31]})
+        f.close()
+    with open(jsonfile,'w+') as p:
+        json.dump(dic,fp=p)
+
+if __name__ == "__main__":
+    getlinesasjson('texts/','data.json')       
